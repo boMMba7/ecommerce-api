1285243ffdaeefcd4085a5cf6244e3ac3518085a
Fabio <fabiopontesdev@gmail.com>
2023-12-27 23:51:47 +0000

    desable token

--- middleware/verifyApiToken.js
+++ middleware/verifyApiToken.js
@@ -1,7 +1,7 @@
 const apiAccessToken = "(: -VERY-STRONG-ENCRYPETED-TOKEN- :)";
 
 const verifyApiToken = (req, res, next) => {
-    if (req.headers.apitoken !== apiAccessToken) return res.sendStatus(401);
+    // if (req.headers.apitoken !== apiAccessToken) return res.sendStatus(401);
     next();
 };
 
